094390a9de79df966f256355400010dc7b1fce95
hamza <elfarre.hamza@gmail.com>
2020-06-16 19:04:36 +0100

    fixed routing issue

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/TradeHome'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/TradeHomeindex.html'));
+res.sendFile(path.join(__dirname+'/dist/TradeHome/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
