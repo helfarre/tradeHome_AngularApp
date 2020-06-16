01dc97cbcc8e3a6eebfdaafd5478e6723bf56e75
hamza <elfarre.hamza@gmail.com>
2020-06-16 18:42:52 +0100

    new stuff1

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const app = express();
-const path = require('path'); 
-app.use(express.static(__dirname + '/dist' ));
 
-app.listen(process.env.PORT  ||  8080);
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-console.log('Console listening!');
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/TradeHome'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/TradeHomeindex.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
